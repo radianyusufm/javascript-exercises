fa19c1ef31cc9ee774b259f7704113e98c3be015
radianyusufm <radian.ym@gmail.com>
2024-03-15 01:27:13 +0700

    exercise 8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	temp = 0
+  for (let i = 0; i < arr.length; i++){
+    temp += arr[i]
+  }
+  return temp
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  temp = 1
+  for (let i = 0; i < arr.length; i++){
+    temp *= arr[i]
+  }
+  return temp
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	temp = 1
+  for(let i = 1; i <= n; i++){
+    temp *= i
+  }
+  return temp
 };
 
 // Do not edit below this line
