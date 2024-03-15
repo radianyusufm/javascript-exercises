a2bf18aecf2a2ed2d4f7f7ab98f92bf7ffbfc622
radianyusufm <radian.ym@gmail.com>
2024-03-15 17:19:59 +0700

    fibonacci -10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fib = [0,1]
 
+    if (n == 0){
+        return 0
+    } else if (n == 1){
+        return 1
+    } else if (n < 0 ){
+        return 'OOPS';
+    } else {
+        for (let i = 2; i <= n; i++){
+            fib.push(fib[fib.length-2] + fib[fib.length-1])
+        }
+    }
+
+    return fib[fib.length-1]
 };
 
 // Do not edit below this line
